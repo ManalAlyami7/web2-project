6a187d7b23c4620dc634cd53290d0fe83ab65e88
mayalnajim <128479559+mayalnajim@users.noreply.github.com>
2023-06-01 18:14:37 +0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,1 +1,14 @@
 <?php
+
+include_once 'db.con.php';
+$property_id = $_REQUEST['id'];
+
+$query = "DELETE FROM rentalapplication WHERE property_id=$property_id";
+mysqli_query($conn, $query);
+
+$query = "DELETE FROM propertyimage WHERE property_id=$property_id";
+mysqli_query($conn, $query);
+
+$query = "DELETE FROM property WHERE id=$property_id";
+mysqli_query($conn, $query);
+echo "Property has been deleted";
